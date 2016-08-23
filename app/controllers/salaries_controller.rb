437a3a64d784219f65d0d03e8319faaff106e443
Nguyen Thanh Luan <nguyen.thanh.luan@framgia.com>
2016-08-24 01:56:19 +0700

    base_salary_update

--- app/controllers/salaries_controller.rb
+++ app/controllers/salaries_controller.rb
@@ -4,4 +4,23 @@ class SalariesController < ApplicationController
   def index
     @salaries = Salary.includes_categories.all
   end
+
+  def update
+    if @salary.update_attributes salary_params
+      flash[:success] = flash_message "updated"
+    end
+    redirect_to :back
+  end
+
+  def destroy
+    if @salary.destroy
+      flash[:success] = flash_message "deleted"
+    end
+    redirect_to :back
+  end
+
+  private
+  def salary_params
+    params.require(:salary).permit Salary::ATTRIBUTES_PARAMS
+  end
 end
--- app/models/salary.rb
+++ app/models/salary.rb
@@ -1,4 +1,7 @@
 class Salary < Employee
+  ATTRIBUTES_PARAMS = [:base_salary, :insurance, :personal_deduction,
+    :number_of_dependence]
+
   scope :includes_categories, -> {includes :category}
   class << self
     def all
